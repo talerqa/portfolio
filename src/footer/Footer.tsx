3e83732d1d7defae76c09d853303b8f2db37a1cf
talerqa <hitri3003@gmail.com>
2023-08-13 21:24:12 +0300

    ref: refactoring

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Footer.module.scss'
 import stylesContainer from '../common/styles/Container.module.css';
 
+
 export const Footer = () => {
   return (
     <div className={s.footerBlock}>
